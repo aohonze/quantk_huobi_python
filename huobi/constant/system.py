cc61fd3e5739e1460dce555c30930c75c8e826fd
A ZE <wutengda@pku.edu.cn>
2021-08-21 16:55:17 +0800

    update .gitignore

--- huobi/constant/system.py
+++ huobi/constant/system.py
@@ -8,11 +8,14 @@ HUOBI_WEBSOCKET_URI_PRO = "wss://api.huobi.pro"
 HUOBI_WEBSOCKET_URI_VN = "wss://api.huobi.vn"
 HUOBI_WEBSOCKET_URI_SO = "wss://api.huobi.so"
 
+
 class WebSocketDefine:
     Uri = HUOBI_WEBSOCKET_URI_PRO
 
+
 class RestApiDefine:
     Url = HUOBI_URL_PRO
+
 
 class HttpMethod:
     GET = "GET"
@@ -24,6 +27,7 @@ class HttpMethod:
 class ApiVersion:
     VERSION_V1 = "v1"
     VERSION_V2 = "v2"
+
 
 def get_default_server_url(user_configed_url):
     if user_configed_url and len(user_configed_url):
